99f45542c87e9ed21081be3d2a5d8b43a46d248c
heltonbc <heltondeveloper01@gmail.com>
2022-08-19 15:21:55 -0400

    update home page, home update. to adjust components e new components was added. changed from image to card components.

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,43 +2,41 @@ import React from "react";
 import "../App.scss";
 import Carousel from "react-bootstrap/Carousel";
 import "./Home.scss";
+import home from "./folder-projects/home.jpg";
+import { Button, Card, CardGroup, Col, Row } from "react-bootstrap";
+import { GoMarkGithub } from "react-icons/go";
 
 function Home() {
     return (
-        <Carousel variant="dark" className="p-3">
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src="https://res.cloudinary.com/dz209s6jk/image/upload/q_auto,w_700/Challenges/mflwrbyf8kjvqvpxq9uj.jpg"
-                    alt="First slide"
-                />
-                <Carousel.Caption className="text mb-lg-5">
-                    <h3>Challenges for Beginners</h3>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src="https://res.cloudinary.com/dz209s6jk/image/upload/q_auto,w_700/Challenges/cfr9ssywcgjpcg7or928.jpg"
-                    alt="Second slide"
-                />
-
-                <Carousel.Caption className="text mb-lg-5">
-                    <h3>Challenges for intermediaries</h3>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src="https://res.cloudinary.com/dz209s6jk/image/upload/q_auto,w_700/Challenges/hfqgixemrcybrmoomqsz.jpg"
-                    alt="Third slide"
-                />
-
-                <Carousel.Caption className="text mb-lg-5">
-                    <h3>Challenges for advanced</h3>
-                </Carousel.Caption>
-            </Carousel.Item>
-        </Carousel>
+        <CardGroup>
+            <Row className="m-5">
+                <Col xs={12} lg={6}>
+                    <Card className="home-card1">
+                        <Card.Body>
+                            <Card.Title className="home-title">
+                                Improve your front-end coding skills by building real
+                                projects
+                            </Card.Title>
+                            <Card.Text className="home-text">
+                                Solve real-world HTML, CSS and JavaScript challenges
+                                whilst working to professional designs. Join 423.453
+                                developers building projects, reviewing code, and helping
+                                each other get better.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <Button className="btn-home" size="sm">
+                        Login with GitHub
+                        <GoMarkGithub className="m-1" size={20} color="#FFF" />
+                    </Button>
+                </Col>
+                <Col xs={12} lg={6}>
+                    <Card className="m-2">
+                        <Card.Img variant="top" src={home} />
+                    </Card>
+                </Col>
+            </Row>
+        </CardGroup>
     );
 }
 
